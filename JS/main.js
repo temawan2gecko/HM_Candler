1990291f8b51f3c9b3cd9b91e5c037ba830eb92e
Artem Chalunin <145147686+temawan2gecko@users.noreply.github.com>
2024-01-04 17:50:10 +0300

    First__commit

---
+++ JS/main.js
@@ -0,0 +1,6 @@
+const menuBtn = document.querySelector(".header__nav__btn");
+const menuMobile = document.querySelector(".header__menu__list");
+
+menuBtn.addEventListener("click", () => {
+  menuMobile.classList.toggle("menu__open");
+});
